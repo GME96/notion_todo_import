58ae415fd8335f9689617c40a0cfcaef3283fd0f
Georg Edegger <georg@DESKTOP-5ODM3RG.mehrdata.local>
2020-05-30 18:26:46 +0200

    asdf

--- app.py
+++ app.py
@@ -61,14 +61,14 @@ def createEntryHabitTracker(token, date, string_date):
     row.title = date[9:10]
     row.date = date.strptime(date[:10], '%Y-%m-%d')
 
-def createEntryWeeklyPlanner(token, date):
+def createEntryWeeklyPlanner(token, day):
     client = NotionClient(token)
     cv = client.get_collection_view(url_weekly)
     row = cv.collection.add_row()
-    startdate =  date.strptime(date[:10], '%Y-%m-%d')
+    startdate =  date.strptime(day[:10], '%Y-%m-%d')
     enddate = startdate + timedelta(days=7)
     enddatestring = enddate.strftime("%d")
-    title_text = date[9:10]  + ' - ' + enddatestring + '.' +  date[6:7] + '.' + date[0:4] ##convert bis date properly
+    title_text = day[9:10]  + ' - ' + enddatestring + '.' +  day[6:7] + '.' + day[0:4] ##convert bis date properly
     row.title = title_text
     row.startdate = startdate
     row.enddate = enddate
